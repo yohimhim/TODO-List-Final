5aa5a9975473afac33ec56cb08571d5db516e741
Marcus <yo.himhim@gmail.com>
2025-08-05 10:39:41 -0500

    get tasks

--- todoBackend/src/main/java/com/yohimhim/todoBackend/controller/TaskController.java
+++ todoBackend/src/main/java/com/yohimhim/todoBackend/controller/TaskController.java
@@ -3,12 +3,14 @@ package com.yohimhim.todoBackend.controller;
 import com.yohimhim.todoBackend.model.Task;
 import com.yohimhim.todoBackend.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class TaskController {
 
     @Autowired
